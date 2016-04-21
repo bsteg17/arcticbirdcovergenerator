f0e19814c8424a8b17422c41a05e0b6d0f12b500
bsteg17 <bsteg17@gmail.com>
2016-04-21 15:45:43 -0500

    can add images, input exists for title, author, color (no functionality)

---
+++ client.js
@@ -0,0 +1,26 @@
+var image = new Image();
+var title;
+var author;
+var color;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+//canvas initialization
+ctx.fillStyle = 'blue';
+ctx.fillRect(10,10,200,200);
+
+function handleFileSelect(evt) {
+    image.src = URL.createObjectURL(evt.target.files[0]);
+}
+
+function onImageLoad() {
+    ctx.drawImage(image, 0, 0);
+}
+
+//resize canvas to fit window
+canvas.setAttribute('height', window.innerHeight);
+canvas.setAttribute('width', window.innerWidth);
+
+//event handler initializations
+document.getElementById('image-upload').addEventListener('change', handleFileSelect, false);
+image.onload = onImageLoad;
---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+
+var PORT = 8000;
+
+server = http.createServer(function(req, res) {
+    if (req.url == '/') {
+        homepage = fs.readFileSync("index.html");
+        res.end(homepage);
+    }
+    if (req.url == '/client.js') {
+        js = fs.readFileSync('client.js');
+        res.end(js);
+    }
+}).listen(PORT);
+
+console.log("server is listening on 8000");
